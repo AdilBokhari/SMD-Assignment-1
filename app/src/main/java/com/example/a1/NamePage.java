1c5cc02fd921655300c6943da09282d777dababe
Adil <bokhariadil@gmail.com>
2025-02-14 16:06:20 +0500

    Splash Screen, Name Page

--- app/src/main/java/com/example/a1/NamePage.java
+++ app/src/main/java/com/example/a1/NamePage.java
@@ -1,11 +1,8 @@
 package com.example.a1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,36 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class NamePage extends AppCompatActivity {
 
-
-    ImageView ivLogo;
-    Animation fade_anim;
+    Button btnNamePage;
+    EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_name_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         init();
 
-        fade_anim = AnimationUtils.loadAnimation(this, R.anim.fade_in_logo);
-        ivLogo.setAnimation(fade_anim);
-
-        new Handler().postDelayed(()->{
-            startActivity(new Intent(MainActivity.this, NamePage.class));
-            finish();
-        },5000);
     }
 
     private void init(){
-        ivLogo=findViewById(R.id.ivLogo);
+        btnNamePage=findViewById(R.id.btnNamePage);
+        etName=findViewById(R.id.etName);
     }
-
 }
